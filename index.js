dd93db0a21fde7a8e2d8817d617e37faf173cd8c
Omar Qaqish <omqaqish@gmail.com>
2023-04-05 15:59:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string) {
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return string.replace(/\w\S*/g, function(word, index) {
+      if (index > 0 && smallWords.includes(word.toLowerCase())) {
+        return word.toLowerCase();
+      }
+      return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+    });
+  }
 }
+
+console.log(Formatter.titleize("a tale OF two cities"));
+console.log(Formatter.titleize("Maurice a an but of and for at by from end"));
+console.log(Formatter.capitalize("BURSTING BALLOON"));
+console.log(Formatter.sanitize("Entert*ain(i{ng-Elephan^ts"))
